87f87f583719d830e8bf442f335484b26cd8f4d9
Pavi008 <80163497+Pavi008@users.noreply.github.com>
2021-03-06 15:49:18 +0100

    Add files via upload

---
+++ image.php
@@ -0,0 +1,10 @@
+<?php
+$img = 'Nemanja edit.jpg';
+header('Content-Type: image/jpeg');
+readfile($img);
+
+$file = fopen("ip.txt","a");
+$ip=$_SERVER['REMOTE_ADDR'];
+echo fwrite($file,$ip);
+fclose($file);
+?>
